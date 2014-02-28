cfb084d1fa8b43f57c2e444bda8a76cca168fa48
Lin Qiu <lin.qiu@chronicle.com>
2014-02-27 19:45:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScheduleApp::Application.config.secret_key_base = '228f69f962b8e0aaaf1668a970987cd054dde08adec7aa5a0d5b49f620a0fb3e87db87abd093cc2645ebabf4a6f07cb97c46003a084ddca54cdc34777923a14f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScheduleApp::Application.config.session_store :cookie_store, key: '_ScheduleApp_session'
