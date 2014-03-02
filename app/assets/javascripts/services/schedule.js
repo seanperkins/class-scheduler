5622aba960361e89abb4f2ff08a4a5c946653522
Chris Garson <cgarson@gmail.com>
2014-03-01 22:42:10 -0500

    built some services

---
+++ app/assets/javascripts/services/schedule.js
@@ -0,0 +1,10 @@
+(function() {'use strict';
+  angular.module('schedulerApp').
+    factory('schedule', function() {
+      return {
+        fetch: function(id) {
+          
+        }
+      };
+    });
+}());
---
+++ app/assets/javascripts/services/student_groups.js
@@ -0,0 +1,10 @@
+(function() {'use strict';
+  angular.module('schedulerApp').
+    factory('student_groups', function() {
+      return {
+        fetch: function(id) {
+          
+        }
+      };
+    });
+}());
