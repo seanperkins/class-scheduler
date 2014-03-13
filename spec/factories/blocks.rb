8e1a5d1d4439cd502aa3d4516434cd11c6d1185d
Lin Qiu <lin.qiu@chronicle.com>
2014-03-13 13:33:01 -0400

    Fix spec tests due to updates

--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -2,8 +2,5 @@ FactoryGirl.define do
   factory :block do
     duration    30
     start_time  1
-    end_time    3
-    immutable   false
-    day         "Thursday"
   end
 end
--- spec/features/user_can_visit_home_page_spec.rb
+++ spec/features/user_can_visit_home_page_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe "An anonymous user" do
   describe "visits homepage" do
-    
+
     it "should see welcome message" do
       visit "/"
 
-      page.should have_content "hello!"
+      page.should have_content "Student Groups"
     end
   end
 end
